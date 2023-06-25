9ad31d2f946c32f062840979f39fdcef403b1e93
abanerjee1407 <abanerjee@lula.life>
2023-06-25 16:40:09 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,108 @@
-import Image from 'next/image'
+"use client";
+import useLLM from "usellm";
+import { useState } from "react";
 
 export default function Home() {
+  const llm = useLLM();
+  const [genere, setGenere] = useState("");
+  const [mood, setMood] = useState("");
+  const [type, setType] = useState("");
+  const [age, setAge] = useState("");
+  const [theme, setTheme] = useState("");
+  const [duration, setDuration] = useState("");
+  const [actors, setActors] = useState("");
+  const [result, setResult] = useState("");
+
+  async function handleClick() {
+    try {
+      await llm.chat({
+        messages: [
+          {
+            role: "system",
+            content: `Your name is Bingy, you are an AI bot powered by ChatGPT and engineerd by Team RAS. You are responsible for suggesting movie and series names based on
+            few parameters provided by the user. You have to analyze the mood and personality of user based on these parameters and suggest a suitable movie or TV series available over the internet.
+            Please refrain from answering any other questions other than movie or TV series related. In case user asks any off the topic questions politely decline to answer that.`,
+          },
+          { role: "user", 
+          content: `Please suugest me what to watch with the following preferences: 
+          genere = ${genere},
+          mood of the item = ${mood || "Any"}
+          movie or series = ${type || "Any"}
+          age of the item = ${age || "Any"}
+          theme of the item = ${theme || "Any"}
+          I have time for = ${duration || "Any"}
+          my preferred actor(s) = ${actors || "Any"}
+          ` },
+        ],
+        stream: true,
+        onStream: ({ message }) => setResult(message.content),
+      });
+    } catch (error) {
+      console.error("Something went wrong!", error);
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="min-h-screen mx-auto my-8 max-w-4xl">
+      <h1 className="text-center mb-4 text-2xl">BingeToday!</h1>
+      <div className="flex flex-col">
+        <input
+          value={genere}
+          onChange={(e) => setGenere(e.target.value)}
+          placeholder="Mention genere that you prefer? (e.g., action, comedy, drama, sci-fi)"
+          style={{ width: '60%' }}
+          className="rounded border p-2 mr-2 mb-2 text-black"
         />
+        <input
+          value={mood}
+          onChange={(e) => setMood(e.target.value)}
+          placeholder="Any specific mood you are looking for? (e.g., suspenseful, uplifting, light-hearted)"
+          style={{ width: '75%' }}
+          className="rounded border p-2 mr-2 mb-2 text-black"
+        />
+        <input
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+          placeholder="Would you prefer a movie or a series?"
+          style={{ width: '35%' }}
+          className="rounded border p-2 mr-2 mb-2 text-black"
+        />
+        <input
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder="Something recent or are you open to older films or series?"
+          style={{ width: '55%' }}
+          className="rounded border p-2 mr-2 mb-2 text-black"
+        />
+        <input
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+          placeholder="Interested in any specific theme? (e.g., historical, fantasy, crime)"
+          style={{ width: '60%' }}
+          className="rounded border p-2 mr-2 mb-2 text-black"
+        />
+        <input
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
+          placeholder="How much time do you have for watching? (e.g., 2 hours, 2-4 hours, binge-watching)"
+          style={{ width: '75%' }}
+          className="rounded border p-2 mr-2 mb-2 text-black"
+        />
+        <input
+          value={actors}
+          onChange={(e) => setActors(e.target.value)}
+          placeholder="Do you have any favorite actors or actresses whose work you particularly enjoy?"
+          style={{ width: '75%' }}
+          className="rounded border p-2 mr-2 mb-2 text-black"
+        />
+        
+        <button
+          className="rounded border border-black dark:border-white p-2"
+          onClick={handleClick}
+        >
+          Submit
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <div className="mt-4 whitespace-pre-wrap">{result}</div>
+    </div>
+  );
 }
